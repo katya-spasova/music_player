c47abbf8a8356869214dc4b89e6506a712b47120
Katya Spasova <katya.spasova@skyscanner.net>
2016-01-10 23:49:54 +0200

    add client test

--- client/playback_contol.go
+++ client/playback_contol.go
@@ -12,14 +12,15 @@ func main() {
 	getAlive()
 }
 
-func getAlive() {
+func getAlive() string {
 	response, err := http.Get(HOST)
 	if err != nil {
 		fmt.Println("The service is not alive")
-		return
+		return ""
 	}
 	defer response.Body.Close()
 	body, err := ioutil.ReadAll(response.Body)
 	text := string(body[:])
 	fmt.Println(text)
+	return text
 }
