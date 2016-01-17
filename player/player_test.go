4fb7135002bfdc9a6d54e8e0285cb9f301954b44
Katya Spasova <katya.spasova11@gmail.com>
2016-01-17 22:55:23 +0200

    player can be play in a separate goroutine

--- player/player_test.go
+++ player/player_test.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestPlaySingleFile(t *testing.T) {
-	player := Player{}
-	player.init()
-	player.playSingleFile("beep9.mp3")
+	wg := sync.WaitGroup{}
+	player = Player{WG: &wg}
+	err := player.init()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	wg.Add(1)
+	start := time.Now()
+	player.playSingleFile("test_sounds/beep9.mp3")
+	wg.Wait()
+	duration := time.Since(start)
+	if duration.Seconds() < 1 {
+		fmt.Println("played file for ", duration.Seconds())
+		t.Errorf("Could not play the sample file")
+	}
 	player.clear()
 }
