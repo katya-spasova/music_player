0b6278f693312a4ca5e101d9d3926ab486e48598
Katya Spasova <katya.spasova11@gmail.com>
2016-01-18 07:43:54 +0200

    from unit tests to integration tests

--- player/service_test.go
+++ player/service_test.go
@@ -3,17 +3,37 @@ package main
 import (
 	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
-func TestGetAlive(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(alive))
-	defer ts.Close()
+const HOST = "http://localhost:8765/"
 
-	res, err := http.Get(ts.URL)
+func TestGetAlive(t *testing.T) {
+	response, err := http.Get(HOST)
 	if err != nil {
-		t.Fatalf(err.Error())
+		t.Fatalf("Unexpected error found - %s", err.Error())
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	found := string(body[:])
+
+	expected := "I'm alive"
+	if found != expected {
+		t.Errorf("Expected\n---\n%s\n---\nbut found\n---\n%s\n---\n", expected, found)
+	}
+}
+
+func TestPlay(t *testing.T) {
+	filename := "test_sounds/beep9.mp3"
+	url := HOST + "play/" + url.QueryEscape(filename)
+
+	client := &http.Client{}
+	request, err := http.NewRequest("PUT", url, nil)
+	res, err := client.Do(request)
+
+	if err != nil {
+		t.Fatalf("Unexpected error found - %s", err.Error())
 	}
 
 	body, err := ioutil.ReadAll(res.Body)
@@ -22,7 +42,7 @@ func TestGetAlive(t *testing.T) {
 		t.Fatalf(err.Error())
 	}
 	found := string(body[:])
-	expected := "I'm alive"
+	expected := "Playing, test_sounds/beep9.mp3!"
 	if found != expected {
 		t.Errorf("Expected\n---\n%s\n---\nbut found\n---\n%s\n---\n", expected, found)
 	}
