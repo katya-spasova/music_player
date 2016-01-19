357b1ee38d9cc6b9ed3660ed37ffad2a2566121b
Katya Spasova <katya.spasova@skyscanner.net>
2016-01-19 07:17:53 +0200

    add tests for play

--- player/service_test.go
+++ player/service_test.go
@@ -9,41 +9,83 @@ import (
 
 const HOST = "http://localhost:8765/"
 
-func TestGetAlive(t *testing.T) {
-	response, err := http.Get(HOST)
-	if err != nil {
-		t.Fatalf("Unexpected error found - %s", err.Error())
+func performCall(method string, url string) (string, error) {
+	var res *http.Response
+	var err error
+	if method == "GET" {
+		res, err = http.Get(url)
+	} else if method == "POST" {
+		res, err = http.Post(url, "text/plain", nil) //todo:
+	} else if method == "PUT" {
+		client := &http.Client{}
+		request, err1 := http.NewRequest("PUT", url, nil)
+		if err1 != nil {
+			return "", err1
+		}
+		res, err = client.Do(request)
 	}
-	defer response.Body.Close()
-	body, err := ioutil.ReadAll(response.Body)
-	found := string(body[:])
-
-	expected := "I'm alive"
-	if found != expected {
-		t.Errorf("Expected\n---\n%s\n---\nbut found\n---\n%s\n---\n", expected, found)
-	}
-}
-
-func TestPlay(t *testing.T) {
-	filename := "test_sounds/beep9.mp3"
-	url := HOST + "play/" + url.QueryEscape(filename)
-
-	client := &http.Client{}
-	request, err := http.NewRequest("PUT", url, nil)
-	res, err := client.Do(request)
 
 	if err != nil {
-		t.Fatalf("Unexpected error found - %s", err.Error())
+		return "", err
 	}
 
 	body, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
 	if err != nil {
-		t.Fatalf(err.Error())
+		return "", err
 	}
 	found := string(body[:])
-	expected := "Playing, test_sounds/beep9.mp3!"
+	return found, nil
+}
+
+func checkResult(method, url, expected string, t *testing.T) {
+	found, err := performCall(method, url)
+	if err != nil {
+		t.Fatalf("Unexpected error found - %s", err.Error())
+	}
+
 	if found != expected {
 		t.Errorf("Expected\n---\n%s\n---\nbut found\n---\n%s\n---\n", expected, found)
 	}
+}
+
+func TestGetAlive(t *testing.T) {
+	expected := "I'm alive"
+	checkResult("GET", HOST, expected, t)
+}
+
+func TestPlay(t *testing.T) {
+	url := HOST + "play/" + url.QueryEscape("test_sounds/beep9.mp3")
+	expected := "{\"Code\":0,\"Message\":\"Success\",\"Info\":\"Started playing\",\"Data\":[\"test_sounds/beep9.mp3\"]}"
+	checkResult("PUT", url, expected, t)
+}
+
+func TestPlayDir(t *testing.T) {
+	url := HOST + "play/" + url.QueryEscape("test_sounds")
+	expected := "{\"Code\":0,\"Message\":\"Success\",\"Info\":\"Started playing\",\"Data\":[\"test_sounds/beep9.mp3\",\"test_sounds/beep28.mp3\",\"test_sounds/beep36.mp3\"]}"
+	checkResult("PUT", url, expected, t)
+}
+
+func TestPlayPlaylist(t *testing.T) {
+	url := HOST + "play/" + url.QueryEscape("sample_playlist")
+	expected := "{\"Code\":0,\"Message\":\"Success\",\"Info\":\"Started playing\",\"Data\":[\"test_sounds/beep9.mp3\",\"test_sounds/beep28.mp3\",\"test_sounds/beep36.mp3\"]}"
+	checkResult("PUT", url, expected, t)
+}
+
+func TestPlayNonExistingFile(t *testing.T) {
+	url := HOST + "play/" + url.QueryEscape("test_sounds/beep1.mp3")
+	expected := "{\"Code\":4,\"Message\":\"File cannot be found\"}"
+	checkResult("PUT", url, expected, t)
+}
+
+func TestPlayInvalidFileFormat(t *testing.T) {
+	url := HOST + "play/" + url.QueryEscape("test_broken/abc.txt")
+	expected := "{\"Code\":7,\"Message\":\"Format is not supported\"}"
+	checkResult("PUT", url, expected, t)
+}
+
+func TestPlayBrokenFile(t *testing.T) {
+	url := HOST + "play/" + url.QueryEscape("test_broken/abc.txt")
+	expected := "{\"Code\":2,\"Message\":\"SoX failed to open input file\"}"
+	checkResult("PUT", url, expected, t)
 }
