43df1e3b8835168bb8b1e2880c5dbf636d1256b8
Katya Spasova <katya.spasova11@gmail.com>
2016-05-06 18:39:25 +0300

    add play in web ui

--- secret/script/music_player.js
+++ secret/script/music_player.js
@@ -1,5 +1,8 @@
 function init() {
-    document.getElementById("play").addEventListener("click", playSong);
+    document.getElementById("play").addEventListener("click", function(event){
+        event.preventDefault();
+        playSong();
+    });
     document.getElementById("add").addEventListener("click", addSong);
     document.getElementById("stop").addEventListener("click", stopSong);
     document.getElementById("previous").addEventListener("click", previousSong);
@@ -10,10 +13,13 @@ function init() {
 
 
 function playSong() {
-    var name = encodeURIComponent(document.getElementById("name"));
-    xhttp.open("POST", "play/".concat(name), true);
+    var nameElement = document.getElementById("name");
+    var name = encodeURIComponent(nameElement.value);
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("PUT", "play/".concat(name), true);
     xhttp.send();
 }
 
-document.onload = init();
-
+document.addEventListener('DOMContentLoaded', function() {
+   init();
+}, false);
